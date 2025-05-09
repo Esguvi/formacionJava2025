d99a10bd4dec1b67abedfefba4850acf90c8c819
Victor <117128054+Esguvi@users.noreply.github.com>
2025-05-09 13:02:18 +0200

    Update Recursos.java
    - Added extra code that can take more than a barcode in order to analyze and extract all the information of the image that has several barcodes

--- src/main/java/com/edisa/formacion/mayo2025/Recursos.java
+++ src/main/java/com/edisa/formacion/mayo2025/Recursos.java
@@ -5,18 +5,20 @@ import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.multi.GenericMultipleBarcodeReader;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 import javax.imageio.ImageIO;
 import javax.ws.rs.*;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Path("/api")
@@ -89,20 +91,28 @@ public class Recursos {
     public Response leerCodigoBarras(InputStream imagenCargada) {
         try {
             BufferedImage bf = ImageIO.read(imagenCargada);
-
             LuminanceSource ls = new BufferedImageLuminanceSource(bf);
             BinaryBitmap bbitmap = new BinaryBitmap(new HybridBinarizer(ls));
-            Result result = new MultiFormatReader().decode(bbitmap);
 
-            Map<String, String> resultado = new HashMap<>();
-            resultado.put("texto", result.getText());
+            MultiFormatReader mfr = new MultiFormatReader();
 
-            return Response.ok(resultado).build();
+            GenericMultipleBarcodeReader reader = new GenericMultipleBarcodeReader(mfr);
+            Result[] results = reader.decodeMultiple(bbitmap);
+
+            List<Map<String, String>> resultados = new ArrayList<>();
+            for (Result result : results) {
+                Map<String, String> resultado = new HashMap<>();
+                resultado.put("texto", result.getText());
+                resultados.add(resultado);
+            }
+
+            return Response.ok(resultados).build();
 
         } catch (Exception e) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error al procesar la imagen: " + e.getMessage()).build();
         }
     }
+
 
     @POST
     @Path("/persona")
